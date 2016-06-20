41d3f6d81d1bfaf96239ba58d4bb086a988127ea
supunkamburugamuve <supun06@gmail.com>
2016-06-20 00:23:06 -0400

    initial

---
+++ server.cpp
@@ -0,0 +1,13 @@
+#include <rdma/fabric.h>
+#include <rdma/fi_domain.h>
+#include <rdma/fi_endpoint.h>
+#include <rdma/fi_cm.h>
+#include <rdma/fi_tagged.h>
+#include <rdma/fi_rma.h>
+#include <rdma/fi_errno.h>
+
+#include "server.h"
+
+void RDMAServer::StartServer() {
+
+}
---
+++ server.h
@@ -0,0 +1,10 @@
+#include <list>
+#include <map>
+#include <utility>
+#include <vector>
+
+class RDMAServer {
+  public:
+    void StartServer();
+};
+
