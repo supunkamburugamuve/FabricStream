4c8238b253398842586c69190ef1379b44c68750
supunkamburugamuve <supun06@gmail.com>
2016-07-01 00:16:28 -0400

    add

--- client.cpp
+++ client.cpp
@@ -58,6 +58,287 @@ int RDMACLient::OpenFabric2(void) {
 	return 0;
 }
 
+#define FT_POST(post_fn, comp_fn, seq, op_str, ...)				\
+	do {									\
+		int timeout_save;						\
+		int ret, rc;							\
+										\
+		while (1) {							\
+			ret = post_fn(__VA_ARGS__);				\
+			if (!ret)						\
+				break;						\
+										\
+			if (ret != -FI_EAGAIN) {				\
+				printf("%s %d\n", op_str, ret);			\
+				return ret;					\
+			}							\
+										\
+			timeout_save = timeout;					\
+			timeout = 0;						\
+			rc = comp_fn(seq);					\
+			if (rc && rc != -FI_EAGAIN) {				\
+				printf("Failed to get %s completion\n", op_str);	\
+				return rc;					\
+			}							\
+			timeout = timeout_save;					\
+		}								\
+		seq++;								\
+	} while (0)
+
+/*
+ * fi_cq_err_entry can be cast to any CQ entry format.
+ */
+int RDMACLient::SpinForCompletion(struct fid_cq *cq, uint64_t *cur,
+			    uint64_t total, int timeout)
+{
+	struct fi_cq_err_entry comp;
+	struct timespec a, b;
+	int ret;
+
+	if (timeout >= 0)
+		clock_gettime(CLOCK_MONOTONIC, &a);
+
+	while (total - *cur > 0) {
+		ret = fi_cq_read(cq, &comp, 1);
+		if (ret > 0) {
+			if (timeout >= 0)
+				clock_gettime(CLOCK_MONOTONIC, &a);
+
+			(*cur)++;
+		} else if (ret < 0 && ret != -FI_EAGAIN) {
+			return ret;
+		} else if (timeout >= 0) {
+			clock_gettime(CLOCK_MONOTONIC, &b);
+			if ((b.tv_sec - a.tv_sec) > timeout) {
+				fprintf(stderr, "%ds timeout expired\n", timeout);
+				return -FI_ENODATA;
+			}
+		}
+	}
+
+	return 0;
+}
+
+/*
+ * fi_cq_err_entry can be cast to any CQ entry format.
+ */
+int RDMACLient::WaitForCompletion(struct fid_cq *cq, uint64_t *cur,
+			    uint64_t total, int timeout)
+{
+	struct fi_cq_err_entry comp;
+	int ret;
+
+	while (total - *cur > 0) {
+		ret = fi_cq_sread(cq, &comp, 1, NULL, timeout);
+		if (ret > 0)
+			(*cur)++;
+		else if (ret < 0 && ret != -FI_EAGAIN)
+			return ret;
+	}
+
+	return 0;
+}
+
+/*
+ * fi_cq_err_entry can be cast to any CQ entry format.
+ */
+int RDMACLient::FDWaitForComp(struct fid_cq *cq, uint64_t *cur,
+			    uint64_t total, int timeout)
+{
+	struct fi_cq_err_entry comp;
+	struct fid *fids[1];
+	int fd, ret;
+
+	fd = cq == txcq ? tx_fd : rx_fd;
+	fids[0] = &cq->fid;
+
+	while (total - *cur > 0) {
+		ret = fi_trywait(fabric, fids, 1);
+		if (ret == FI_SUCCESS) {
+			ret = rdma_utils_poll_fd(fd, timeout);
+			if (ret && ret != -FI_EAGAIN)
+				return ret;
+		}
+
+		ret = fi_cq_read(cq, &comp, 1);
+		if (ret > 0) {
+			(*cur)++;
+		} else if (ret < 0 && ret != -FI_EAGAIN) {
+			return ret;
+		}
+	}
+
+	return 0;
+}
+
+int RDMACLient::GetCQComp(struct fid_cq *cq, uint64_t *cur,
+			  uint64_t total, int timeout) {
+	int ret;
+
+	switch (this->options->comp_method) {
+	case FT_COMP_SREAD:
+		ret = WaitForCompletion(cq, cur, total, timeout);
+		break;
+	case FT_COMP_WAIT_FD:
+		ret = FDWaitForComp(cq, cur, total, timeout);
+		break;
+	default:
+		ret = SpinForCompletion(cq, cur, total, timeout);
+		break;
+	}
+
+	if (ret) {
+		if (ret == -FI_EAVAIL) {
+			ret = rdma_utils_cq_readerr(cq);
+			(*cur)++;
+		} else {
+			printf("ft_get_cq_comp %d\n", ret);
+		}
+	}
+	return ret;
+}
+
+int RDMACLient::GetRXComp(uint64_t total) {
+	int ret = FI_SUCCESS;
+
+	if (rxcq) {
+		ret = GetCQComp(rxcq, &rx_cq_cntr, total, timeout);
+	} else if (rxcntr) {
+		while (fi_cntr_read(rxcntr) < total) {
+			ret = fi_cntr_wait(rxcntr, total, timeout);
+			if (ret)
+				printf("fi_cntr_wait %d\n", ret);
+			else
+				break;
+		}
+	} else {
+		printf("Trying to get a RX completion when no RX CQ or counter were opened");
+		ret = -FI_EOTHER;
+	}
+	return ret;
+}
+
+int RDMACLient::GetTXComp(uint64_t total) {
+	int ret;
+
+	if (txcq) {
+		ret = GetCQComp(txcq, &tx_cq_cntr, total, -1);
+	} else if (txcntr) {
+		ret = fi_cntr_wait(txcntr, total, -1);
+		if (ret)
+			printf("fi_cntr_wait %d\n", ret);
+	} else {
+		printf("Trying to get a TX completion when no TX CQ or counter were opened \n");
+		ret = -FI_EOTHER;
+	}
+	return ret;
+}
+
+ssize_t RDMACLient::PostTX(struct fid_ep *ep, fi_addr_t fi_addr, size_t size, struct fi_context* ctx) {
+	if (info_hints->caps & FI_TAGGED) {
+		FT_POST(fi_tsend, GetTXComp, tx_seq, "transmit", ep,
+				tx_buf, size + rdma_utils_tx_prefix_size(info), fi_mr_desc(mr),
+				fi_addr, tx_seq, ctx);
+	} else {
+		FT_POST(fi_send, GetTXComp, tx_seq, "transmit", ep,
+				tx_buf,	size + rdma_utils_tx_prefix_size(info), fi_mr_desc(mr),
+				fi_addr, ctx);
+	}
+	return 0;
+}
+
+ssize_t RDMACLient::TX(struct fid_ep *ep, fi_addr_t fi_addr, size_t size, struct fi_context *ctx)
+{
+	ssize_t ret;
+
+	if (rdma_utils_check_opts(options, FT_OPT_VERIFY_DATA | FT_OPT_ACTIVE))
+		rdma_utils_fill_buf((char *) tx_buf + rdma_utils_tx_prefix_size(info), size);
+
+	ret = PostTX(ep, fi_addr, size, ctx);
+	if (ret)
+		return ret;
+
+	ret = GetTXComp(tx_seq);
+	return ret;
+}
+
+ssize_t RDMACLient::PostRX(struct fid_ep *ep, size_t size, struct fi_context* ctx)
+{
+	if (info_hints->caps & FI_TAGGED) {
+		FT_POST(fi_trecv, GetRXComp, rx_seq, "receive", ep, rx_buf,
+				MAX(size, FT_MAX_CTRL_MSG) + rdma_utils_rx_prefix_size(info),
+				fi_mr_desc(mr), 0, rx_seq, 0, ctx);
+	} else {
+		FT_POST(fi_recv, GetRXComp, rx_seq, "receive", ep, rx_buf,
+				MAX(size, FT_MAX_CTRL_MSG) + rdma_utils_rx_prefix_size(info),
+				fi_mr_desc(mr),	0, ctx);
+	}
+	return 0;
+}
+
+ssize_t RDMACLient::RX(struct fid_ep *ep, size_t size) {
+	ssize_t ret;
+
+	ret = GetRXComp(rx_seq);
+	if (ret)
+		return ret;
+
+	if (rdma_utils_check_opts(options, FT_OPT_VERIFY_DATA | FT_OPT_ACTIVE)) {
+		ret = rdma_utils_check_buf((char *) rx_buf + rdma_utils_rx_prefix_size(info), size);
+		if (ret)
+			return ret;
+	}
+	/* TODO: verify CQ data, if available */
+
+	/* Ignore the size arg. Post a buffer large enough to handle all message
+	 * sizes. ft_sync() makes use of ft_rx() and gets called in tests just before
+	 * message size is updated. The recvs posted are always for the next incoming
+	 * message */
+	ret = PostRX(ep, rx_size, &rx_ctx);
+	return ret;
+}
+
+int RDMACLient::ExchangeKeys(struct fi_rma_iov *peer_iov) {
+	struct fi_rma_iov *rma_iov;
+	int ret;
+
+	if (options->dst_addr) {
+		rma_iov = (fi_rma_iov *)(tx_buf + rdma_utils_tx_prefix_size(info));
+		rma_iov->addr = info->domain_attr->mr_mode == FI_MR_SCALABLE ?
+				0 : (uintptr_t) rx_buf + rdma_utils_rx_prefix_size(info);
+		rma_iov->key = fi_mr_key(mr);
+		ret = TX(ep, remote_fi_addr, sizeof *rma_iov, &tx_ctx);
+		if (ret)
+			return ret;
+
+		ret = GetRXComp(rx_seq);
+		if (ret)
+			return ret;
+
+		rma_iov = (fi_rma_iov *)(rx_buf + rdma_utils_rx_prefix_size(info));
+		*peer_iov = *rma_iov;
+		ret = PostRX(ep, rx_size, &rx_ctx);
+	} else {
+		ret = GetRXComp(rx_seq);
+		if (ret)
+			return ret;
+
+		rma_iov = (fi_rma_iov *)(rx_buf + rdma_utils_rx_prefix_size(info));
+		*peer_iov = *rma_iov;
+		ret = PostRX(ep, rx_size, &rx_ctx);
+		if (ret)
+			return ret;
+
+		rma_iov = (fi_rma_iov *)(tx_buf + rdma_utils_tx_prefix_size(info));
+		rma_iov->addr = info->domain_attr->mr_mode == FI_MR_SCALABLE ?
+				0 : (uintptr_t) rx_buf + rdma_utils_rx_prefix_size(info);
+		rma_iov->key = fi_mr_key(mr);
+		ret = TX(ep, remote_fi_addr, sizeof *rma_iov, &tx_ctx);
+	}
+
+	return ret;
+}
+
 /*
  * Include FI_MSG_PREFIX space in the allocated buffer, and ensure that the
  * buffer is large enough for a control message used to exchange addressing
--- client.h
+++ client.h
@@ -63,6 +63,10 @@ private:
   uint64_t remote_cq_data;
   struct fid_mr *mr;
   struct fid_mr no_mr;
+
+  uint64_t tx_seq, rx_seq, tx_cq_cntr, rx_cq_cntr;
+
+  fi_addr_t remote_fi_addr;
   /**
    * Private methods
    */
@@ -73,6 +77,21 @@ private:
   int InitEp(struct fi_info *hints, struct fi_info *fi);
   int AllocateActiveRes(struct fi_info *hints, struct fi_info *fi);
   int AllocMsgs(void);
+  int ExchangeKeys(struct fi_rma_iov *peer_iov);
+  ssize_t PostTX(struct fid_ep *ep, fi_addr_t fi_addr, size_t size, struct fi_context* ctx);
+  ssize_t PostRX(struct fid_ep *ep, size_t size, struct fi_context* ctx);
+  ssize_t TX(struct fid_ep *ep, fi_addr_t fi_addr, size_t size, struct fi_context *ctx);
+  ssize_t RX(struct fid_ep *ep, size_t size);
+  int GetTXComp(uint64_t total);
+  int GetRXComp(uint64_t total);
+  int GetCQComp(struct fid_cq *cq, uint64_t *cur,
+  			  uint64_t total, int timeout);
+  int FDWaitForComp(struct fid_cq *cq, uint64_t *cur,
+  			    uint64_t total, int timeout);
+  int WaitForCompletion(struct fid_cq *cq, uint64_t *cur,
+  			    uint64_t total, int timeout);
+  int SpinForCompletion(struct fid_cq *cq, uint64_t *cur,
+  			    uint64_t total, int timeout);
 };
 
 #endif
