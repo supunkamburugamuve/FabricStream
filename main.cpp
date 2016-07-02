b9c04b399f98e8ac0b8eb11e28646e38370a1fae
supunkamburugamuve <supun06@gmail.com>
2016-07-01 20:49:42 -0400

    add

--- main.cpp
+++ main.cpp
@@ -13,6 +13,39 @@ using namespace std;
 
 static struct fi_rma_iov remote;
 
+struct test_size_param {
+	int size;
+	int enable_flags;
+};
+
+#define FT_DEFAULT_SIZE		(1 << 0)
+
+struct test_size_param test_size[] = {
+	{ 1 <<  1, 0 }, { (1 <<  1) + (1 <<  0), 0 },
+	{ 1 <<  2, 0 }, { (1 <<  2) + (1 <<  1), 0 },
+	{ 1 <<  3, 0 }, { (1 <<  3) + (1 <<  2), 0 },
+	{ 1 <<  4, 0 }, { (1 <<  4) + (1 <<  3), 0 },
+	{ 1 <<  5, 0 }, { (1 <<  5) + (1 <<  4), 0 },
+	{ 1 <<  6, FT_DEFAULT_SIZE }, { (1 <<  6) + (1 <<  5), 0 },
+	{ 1 <<  7, 0 }, { (1 <<  7) + (1 <<  6), 0 },
+	{ 1 <<  8, FT_DEFAULT_SIZE }, { (1 <<  8) + (1 <<  7), 0 },
+	{ 1 <<  9, 0 }, { (1 <<  9) + (1 <<  8), 0 },
+	{ 1 << 10, FT_DEFAULT_SIZE }, { (1 << 10) + (1 <<  9), 0 },
+	{ 1 << 11, 0 }, { (1 << 11) + (1 << 10), 0 },
+	{ 1 << 12, FT_DEFAULT_SIZE }, { (1 << 12) + (1 << 11), 0 },
+	{ 1 << 13, 0 }, { (1 << 13) + (1 << 12), 0 },
+	{ 1 << 14, 0 }, { (1 << 14) + (1 << 13), 0 },
+	{ 1 << 15, 0 }, { (1 << 15) + (1 << 14), 0 },
+	{ 1 << 16, FT_DEFAULT_SIZE }, { (1 << 16) + (1 << 15), 0 },
+	{ 1 << 17, 0 }, { (1 << 17) + (1 << 16), 0 },
+	{ 1 << 18, 0 }, { (1 << 18) + (1 << 17), 0 },
+	{ 1 << 19, 0 }, { (1 << 19) + (1 << 18), 0 },
+	{ 1 << 20, FT_DEFAULT_SIZE }, { (1 << 20) + (1 << 19), 0 },
+	{ 1 << 21, 0 }, { (1 << 21) + (1 << 20), 0 },
+	{ 1 << 22, 0 }, { (1 << 22) + (1 << 21), 0 },
+	{ 1 << 23, 0 },
+};
+
 void rdma_parseinfo(int op, char *optarg, struct fi_info *hints) {
 	switch (op) {
 	case 'n':
@@ -119,6 +152,7 @@ int main(int argc, char **argv) {
 		}
 
 		for (int i = 0; i < 100; i++) {
+			options.transfer_size = test_size[0];
 			ret = client.RMA(options.rma_op, options.transfer_size, &remote);
 			if (ret) {
 				printf("Failed to RMA \n");
@@ -147,6 +181,7 @@ int main(int argc, char **argv) {
 			printf("Failed to sync\n");
 		}
 		for (int i = 0; i < 100; i++) {
+			options.transfer_size = test_size[0];
 			ret = server.RMA(options.rma_op, options.transfer_size, &remote);
 			if (ret) {
 				printf("Failed to RMA \n");
